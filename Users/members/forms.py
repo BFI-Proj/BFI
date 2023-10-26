bd61a13fcd106c2d2362619d0b31fa37473419e9
PN-Colico <89898620+PN-Colico@users.noreply.github.com>
2023-10-26 14:55:06 +0800

    Update forms.py

--- Users/members/forms.py
+++ Users/members/forms.py
@@ -3,7 +3,7 @@ from .models import FoodItem
 class FoodItemForm(forms.ModelForm):
     class Meta:
         model = FoodItem
-        fields = ['name', 'ingredients', 'category', 'image']  # Include the 'image' field
+        fields = ['name', 'ingredients', 'category', 'image', 'description']  # Include the 'image' field
 
 class FoodItemSearchForm(forms.Form):
     search_query = forms.CharField(max_length=100, label='Search for Food')
