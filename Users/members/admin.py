735df1c9761586face625d6a6335565e5676a861
PN-Colico <89898620+PN-Colico@users.noreply.github.com>
2023-12-07 12:21:10 +0800

    Update admin.py

--- Users/members/admin.py
+++ Users/members/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import UserProfile
 from .models import FoodItem
 from .models import Appointment
-
+from .models import AdminProfile
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(FoodItem)
 admin.site.register(Appointment)
+admin.site.register(AdminProfile)
 
