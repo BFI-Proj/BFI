6fa6b8abb84c133f6419f55805109247a5a9d5b9
PN-Colico <89898620+PN-Colico@users.noreply.github.com>
2023-12-07 12:20:51 +0800

    Update models.py

--- Users/members/models.py
+++ Users/members/models.py
@@ -59,3 +59,22 @@ class Appointment(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.date} - {self.time}"
+    
+
+class AdminProfile(models.Model):
+    
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('prefer_not_to_say', 'Prefer Not to Say'),
+    ]
+
+    full_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone_number = models.CharField(max_length=11)
+    password = models.CharField(max_length=128)  # Assuming you are storing hashed passwords
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.username
